2a81c99e550be02d2975d93f6591ae528c05677d
Daniel adjei <dadjei@quantumgroupgh.com>
2023-06-07 17:13:02 +0000

    refactor code

--- components/dashboard/MyFiles.vue
+++ components/dashboard/MyFiles.vue
@@ -1,21 +1,29 @@
 <template>
-    <div>
-        <v-sheet max-height="85vh" class="pa-3" rounded>
-            <div class="d-flex justify-space-between align-center mb-2">
-                <p class="text-h5 font-weight-light">My files</p>
-                <v-btn size="sm" elevation="1" icon="mdi-plus" rounded="sm" />
-            </div>
-            <v-divider />
+  <div>
+    <v-sheet max-height="85vh" class="pa-3" rounded>
+      <div class="d-flex justify-space-between align-center mb-2">
+        <p class="text-h5 font-weight-light">
+          My files <v-icon icon="mdi-file-document-outline" class="ml-2" />
+        </p>
+        <CreateFileDropDown />
+      </div>
+      <v-divider />
 
-            <div class="mt-2">
-                <FileCard />
-            </div>
-        </v-sheet>
-    </div>
+      <div class="mt-2">
+        <FileCard
+          v-for="(file, key) in fileStore.files"
+          :key="key"
+          :file="file"
+        />
+      </div>
+    </v-sheet>
+  </div>
 </template>
 
 <script setup lang="ts">
+const fileStore = useFileStore();
 
+onBeforeMount(async () => {
+  await fileStore.getFiles();
+});
 </script>
-
-<style scoped></style>
