0e00ffd9bdb30fc0105660897a394f832ad466b6
Daniel adjei <dadjei@quantumgroupgh.com>
2023-06-07 17:13:18 +0000

    add file details view

---
+++ pages/file-[id].vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <NuxtLayout name="dashboard">
+            <v-container>
+                <v-sheet class="pa-10" rounded>
+                    <p>Hello</p>
+                </v-sheet>
+            </v-container>
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: false,
+    middleware: ["auth"]
+})
+const fileStore = useFileStore()
+
+const { id } = useRoute().params
+
+if(id !== fileStore.previewUriToken){
+    navigateTo('/')
+}
+</script>
+
+<style scoped>
+
+</style>
