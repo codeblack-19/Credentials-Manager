f1f51902a6263982d425f02c6d81bba16b17a9db
Daniel adjei <dadjei@quantumgroupgh.com>
2023-06-07 17:09:40 +0000

    create alert component for forms

---
+++ components/FormAlert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="my-1">
+    <p v-if="alertStore.error" class="text-error text-body-1 text-center">{{ alertStore.error }}</p>
+    <p v-if="alertStore.success" class="text-success text-body-1 text-center">
+      {{ alertStore.success }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const alertStore= useAlertStore()
+</script>
