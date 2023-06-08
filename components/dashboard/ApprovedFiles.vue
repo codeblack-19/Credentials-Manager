2a0b81e9624ceed29a6bbd5a1e3db5b620c8ecf5
Daniel adjei <dadjei@quantumgroupgh.com>
2023-06-08 23:59:01 +0000

    create approved public files component

---
+++ components/dashboard/ApprovedFiles.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-sheet height="42vh" rounded class="pa-3">
+      <div class="d-flex justify-space-between align-center mb-2">
+        <p class="text-h5 font-weight-light">Approved Access files</p>
+      </div>
+      <v-divider />
+      <div class="mt-2">
+        <div class="mt-2">
+          <FileCard
+            v-for="(file, key) in fileStore.approvedRequestedFiles"
+            :key="key"
+            :file="file.files"
+          />
+        </div>
+      </div>
+    </v-sheet>
+  </div>
+</template>
+
+<script setup lang="ts">
+const fileStore = useFileStore();
+</script>
